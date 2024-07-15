e2a74b4fae71f6b981c1507948bc037eaa7e5f05
Kai <cognito.learning.now@gmail.com>
2024-07-15 16:17:01 +0100

    gfd

---
+++ story_content/user.js
@@ -0,0 +1,151 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64EYHzLYzMu":
+        Script1();
+        break;
+      case "5tuZ9o0teFP":
+        Script2();
+        break;
+      case "5uuPE92EU5I":
+        Script3();
+        break;
+      case "60KlEvaDYu5":
+        Script4();
+        break;
+      case "5wdncpMeeMS":
+        Script5();
+        break;
+      case "68OCVyByKem":
+        Script6();
+        break;
+      case "5oSod5YZege":
+        Script7();
+        break;
+      case "5cgFSkig0oy":
+        Script8();
+        break;
+      case "6VQwX6LFoMW":
+        Script9();
+        break;
+      case "6QPPpjzWgEB":
+        Script10();
+        break;
+      case "6V8cD2Dw2Dm":
+        Script11();
+        break;
+      case "6qpVDSFXR7T":
+        Script12();
+        break;
+      case "698EFMOkMwB":
+        Script13();
+        break;
+      case "653u66mUg9M":
+        Script14();
+        break;
+      case "6a5nPLyWwdP":
+        Script15();
+        break;
+      case "62m96ReTA9k":
+        Script16();
+        break;
+      case "5raRUZIjp5G":
+        Script17();
+        break;
+      case "6OItZDGYAjk":
+        Script18();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
+function Script4()
+{
+  window.print();
+}
+
+function Script5()
+{
+  window.print();
+}
+
+function Script6()
+{
+  window.print();
+}
+
+function Script7()
+{
+  window.print();
+}
+
+function Script8()
+{
+  window.print();
+}
+
+function Script9()
+{
+  window.print();
+}
+
+function Script10()
+{
+  window.print();
+}
+
+function Script11()
+{
+  window.print();
+}
+
+function Script12()
+{
+  window.print();
+}
+
+function Script13()
+{
+  window.print();
+}
+
+function Script14()
+{
+  window.print();
+}
+
+function Script15()
+{
+  window.print();
+}
+
+function Script16()
+{
+  window.print();
+}
+
+function Script17()
+{
+  window.print();
+}
+
+function Script18()
+{
+  window.print();
+}
+
